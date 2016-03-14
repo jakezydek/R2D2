ed00e7eff9ca1decfb19c2ef41a5924f5b1b8df9
jakezydek <djzydek@hotmail.com>
2016-03-13 20:07:23 -0400

    03132016 added module 09

---
+++ mod09.R
@@ -0,0 +1,20 @@
+head(diamonds)
+?diamonds
+plot(diamonds$price~diamonds$carat,cex=.4,pch=20,col="royalblue1")
+price<-diamonds$price
+carat<-diamonds$carat
+csqr<-carat^2
+dlm<-lm(price~carat+csqr)
+xgrid<-seq(0,5,length=100)
+ygrid<-dlm$coef[1]+dlm$coef[2]*xgrid+dlm$coef[3]*xgrid^2
+lines(xgrid,ygrid,col="firebrick2",lwd=3)
+
+xyplot(price~carat,diamonds,
+       group=diamonds$color,auto.key=T,
+       type="p",alpha=.3,)
+
+ggplot(diamonds,aes(x=carat,y=price,color=color))+
+  geom_point(alpha=.5)+
+  geom_smooth(method=lm,se=F,alpha=.3)+
+  coord_cartesian(ylim=c(0,20000))
+  
